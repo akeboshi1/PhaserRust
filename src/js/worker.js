deed98ce6919ab487ef2fd5d79f6e2596957e8b1
guanxiaomin <327547931@qq.com>
2022-07-15 17:52:13 +0800

    update worker code

--- src/js/worker.js
+++ src/js/worker.js
@@ -1,12 +1,15 @@
-import("../../pkg/wasm").then(module => {
-    onmessage = function (m) {
+onmessage = function (m) {
+    console.log("test11");
+    import("../../pkg/wasm").then(module => {
         module.action("wasm");
         const num = module.wasm_add(10, 5);
         run(num);
-    }
-    function run(num) {
-        postMessage("wasm back");
-        postMessage(num);
-    }
-})
+        function run(num) {
+            postMessage("wasm back");
+            postMessage(num);
+        }
+    })
+}
+
+
 
