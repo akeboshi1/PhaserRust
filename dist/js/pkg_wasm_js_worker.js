106c30afb391812f96b6eb8fe125631a026756d3
guanxiaomin <327547931@qq.com>
2022-07-15 20:16:46 +0800

    update gitignore

--- dist/js/pkg_wasm_js_worker.js
+++
@@ -1,4 +0,0 @@
-(this.webpackChunkRust_wasm=this.webpackChunkRust_wasm||[]).push([["pkg_wasm_js"],{"./pkg/wasm.js":
-/*!*********************!*\
-  !*** ./pkg/wasm.js ***!
-  \*********************/()=>{eval("\n\n//# sourceURL=webpack://Rust-wasm/./pkg/wasm.js?")}}]);
