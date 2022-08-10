6f7975038c8eef890aa165dd852ddeed8d2a2caf
guanxiaomin <327547931@qq.com>
2022-08-10 16:01:38 +0800

    add load image code

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,13 @@
 #![allow(non_snake_case)]
 #[warn(unused_imports)]
 #[warn(unused_must_use)]
+
 extern crate wasm_bindgen;
 
+use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C"{
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
+mod rs;
 
 #[wasm_bindgen]
 pub fn action(input: &str) -> String {
@@ -23,7 +17,7 @@ pub fn action(input: &str) -> String {
         format!("Hello, {}!", input)
     };
 
-    console_log!("Wasm in Worker says: {}",&output);
+    log!("Wasm in Worker says: {}",&output);
 
     output
 }
@@ -54,7 +48,7 @@ pub async fn test() -> u8 {
 // ================ thread(弃用，wasm暂时只支持单线程)
 #[wasm_bindgen]
 pub async fn addThreadTest(){
-        let h1 = thread::spawn(|| async{
+        let h1 = thread::spawn(move|| async{
           foo().await
         });
         h1.join().unwrap();
@@ -110,9 +104,9 @@ use wasm_bindgen::JsCast;
 use web_sys::{ErrorEvent, MessageEvent, WebSocket};
 
 
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
+// macro_rules! log {
+//     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+// }
 
 #[wasm_bindgen(start)]
 pub fn start_websocket() -> Result<(), JsValue> {
@@ -125,19 +119,19 @@ pub fn start_websocket() -> Result<(), JsValue> {
     let onmessage_callback = Closure::<dyn FnMut(_)>::new(move |e: MessageEvent| {
         // Handle difference Text/Binary,...
         if let Ok(abuf) = e.data().dyn_into::<js_sys::ArrayBuffer>() {
-            console_log!("message event, received arraybuffer: {:?}", abuf);
+            log!("message event, received arraybuffer: {:?}", abuf);
             let array = js_sys::Uint8Array::new(&abuf);
             let len = array.byte_length() as usize;
-            console_log!("Arraybuffer received {}bytes: {:?}", len, array.to_vec());
+            log!("Arraybuffer received {}bytes: {:?}", len, array.to_vec());
             // here you can for example use Serde Deserialize decode the message
             // for demo purposes we switch back to Blob-type and send off another binary message
             cloned_ws.set_binary_type(web_sys::BinaryType::Blob);
             match cloned_ws.send_with_u8_array(&vec![5, 6, 7, 8]) {
-                Ok(_) => console_log!("binary message successfully sent"),
-                Err(err) => console_log!("error sending message: {:?}", err),
+                Ok(_) => log!("binary message successfully sent"),
+                Err(err) => log!("error sending message: {:?}", err),
             }
         } else if let Ok(blob) = e.data().dyn_into::<web_sys::Blob>() {
-            console_log!("message event, received blob: {:?}", blob);
+            log!("message event, received blob: {:?}", blob);
             // better alternative to juggling with FileReader is to use https://crates.io/crates/gloo-file
             let fr = web_sys::FileReader::new().unwrap();
             let fr_c = fr.clone();
@@ -145,16 +139,16 @@ pub fn start_websocket() -> Result<(), JsValue> {
             let onloadend_cb = Closure::<dyn FnMut(_)>::new(move |_e: web_sys::ProgressEvent| {
                 let array = js_sys::Uint8Array::new(&fr_c.result().unwrap());
                 let len = array.byte_length() as usize;
-                console_log!("Blob received {}bytes: {:?}", len, array.to_vec());
+                log!("Blob received {}bytes: {:?}", len, array.to_vec());
                 // here you can for example use the received image/png data
             });
             fr.set_onloadend(Some(onloadend_cb.as_ref().unchecked_ref()));
             fr.read_as_array_buffer(&blob).expect("blob not readable");
             onloadend_cb.forget();
         } else if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
-            console_log!("message event, received Text: {:?}", txt);
+            log!("message event, received Text: {:?}", txt);
         } else {
-            console_log!("message event, received Unknown: {:?}", e.data());
+            log!("message event, received Unknown: {:?}", e.data());
         }
     });
     // set message event handler on WebSocket
@@ -163,22 +157,22 @@ pub fn start_websocket() -> Result<(), JsValue> {
     onmessage_callback.forget();
 
     let onerror_callback = Closure::<dyn FnMut(_)>::new(move |e: ErrorEvent| {
-        console_log!("error event: {:?}", e);
+        log!("error event: {:?}", e);
     });
     ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
     onerror_callback.forget();
 
     let cloned_ws = ws.clone();
     let onopen_callback = Closure::<dyn FnMut()>::new(move || {
-        console_log!("socket opened");
+        log!("socket opened");
         match cloned_ws.send_with_str("ping") {
-            Ok(_) => console_log!("message successfully sent"),
-            Err(err) => console_log!("error sending message: {:?}", err),
+            Ok(_) => log!("message successfully sent"),
+            Err(err) => log!("error sending message: {:?}", err),
         }
         // send off binary message
         match cloned_ws.send_with_u8_array(&vec![0, 1, 2, 3]) {
-            Ok(_) => console_log!("binary message successfully sent"),
-            Err(err) => console_log!("error sending message: {:?}", err),
+            Ok(_) => log!("binary message successfully sent"),
+            Err(err) => log!("error sending message: {:?}", err),
         }
     });
     ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
@@ -186,5 +180,18 @@ pub fn start_websocket() -> Result<(), JsValue> {
 
     Ok(())
 }
+
+#[wasm_bindgen]
+pub async fn loadImage(bytes: Uint8Array) -> Result<JsValue, JsValue>{
+    let bytes: &Vec<u8> = &bytes.to_vec();
+    let b = rs::image::rotate(bytes,90);
+    let _b =Uint8Array::from(&b[..]).buffer(); 
+    let promise = js_sys::Promise::resolve(&_b.into());
+    let result = wasm_bindgen_futures::JsFuture::from(promise).await?;
+    Ok(result)
+}
+
+
+
 
 
