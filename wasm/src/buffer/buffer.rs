e0bd6233519dd0c0b1b049f032e4dea25ed0572d
guanxiaomin <327547931@qq.com>
2022-08-31 18:22:26 +0800

    add buffer code

--- wasm/src/buffer/buffer.rs
+++ wasm/src/buffer/buffer.rs
@@ -1,104 +1,93 @@
-use core::sync::atomic::{AtomicUsize, Ordering};
-
+use std::cmp;
 use super::mutref::get_mutable_ref;
 
-pub struct Buffer<'a> {
-    bytes: &'a mut [u8],
-    next: AtomicUsize,
+const DEFAULT_BUF_SIZE: usize = 8 * 1024;
+
+struct RawBuf {
+    bytes: Box<[u8]>,
+}
+pub struct Buffer {
+    buf: RawBuf,
+    reserved: usize,
+    pos: usize,
 }
 
-impl<'a> Buffer<'a> {
-    pub fn init_from_bytes(bytes: &'a mut [u8]) -> Self {
-        Self {
-            bytes,
-            next: AtomicUsize::new(0),
+impl Buffer {
+    pub fn new(mut capsize:usize) -> Self {
+        if capsize == 0 {
+            capsize = DEFAULT_BUF_SIZE;
         }
+        let mut vec_u8 = Vec::with_capacity(capsize);
+        let true_cap = vec_u8.capacity();
+            unsafe {
+                vec_u8.set_len(true_cap);
+            };
+            let raw_buf = RawBuf {
+                bytes: vec_u8.into_boxed_slice(),
+            };
+        Self { buf:raw_buf, 
+               reserved:0,
+               pos : 0,
+            }
+    }
+
+    pub fn bytes(&self)->&[u8]{
+        &self.buf.bytes
+    }
+
+    pub fn bytes_mut(&mut self)->&mut [u8]{
+        &mut self.buf.bytes
+    }
+
+    pub fn operate_len(&self) -> usize{
+        self.buf.bytes.len() - self.pos
     }
 
     pub fn len(&self) -> usize {
-        self.bytes.len()
+        self.buf.bytes.len() - self.reserved
     }
 
-    pub fn remaining(&self) -> usize {
-        self.bytes.len() - self.next.load(Ordering::Relaxed)
+    pub fn consume(&mut self, amt: usize) {
+        self.pos = cmp::min(self.pos + amt, self.buf.bytes.len());
     }
 
-    fn reserve(&'a self, sz: usize) -> usize {
+    fn reserve(&mut self, sz: usize) -> usize {
         if sz == 0 {
             return 0;
         }
-        let mut old = self.next.load(Ordering::Relaxed);
-        loop {
-            let new = old + sz;
-            if new >= self.bytes.len() {
-                return 0;
-            }
-            match self
-                .next
-                .compare_exchange_weak(old, new, Ordering::SeqCst, Ordering::Relaxed)
-            {
-                Ok(_) => return new,
-                Err(new) => old = new,
-            }
+        let mut old = self.reserved;
+
+        let new = old + sz;
+        if new >= self.buf.bytes.len() {
+            return 0;
         }
+        self.reserved = new;
+        return new
     }
 
-    pub fn alloc(&'a self, sz: usize) -> Option<&'a [u8]> {
+    pub fn alloc(&mut self, sz: usize) -> Option<& [u8]> {
         let new = self.reserve(sz);
         if new == 0 {
             None
         } else {
             let old = new - sz;
-            Some(&self.bytes[old..new])
+            Some(&self.buf.bytes[old..new])
         }
     }
     
-    pub fn alloc_mut(&'a self, sz: usize) -> Option<&'a mut [u8]> {
+    pub fn alloc_mut(&mut self, sz: usize) -> Option<& mut [u8]> {
         let new = self.reserve(sz);
         if new == 0 {
             None
         } else {
             let self_mut = unsafe { get_mutable_ref(self) };
             let old = new - sz;
-            Some(&mut self_mut.bytes[old..new])
+            Some(&mut self_mut.buf.bytes[old..new])
         }
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use crate::buffer::buffer::Buffer;
+    pub fn clear(&self) {
+        if self.pos == 0 && self.reserved == 0 { return; }
 
-    #[test]
-    fn it_works() {
-        let mut bytes: [u8; 25] = [0; 25];
-        for n in 0..bytes.len() {
-            bytes[n] = (n % 255) as u8;
-        }
-
-        let memory = Buffer::init_from_bytes(&mut bytes);
-        assert_eq!(memory.len(), 25);
-
-        let maybe_buf0 = memory.alloc(12);
-        assert_ne!(maybe_buf0, None);
-        let buf0 = maybe_buf0.unwrap();
-        assert_eq!(buf0.len(), 12);
-        assert_eq!(buf0[0], 0);
-        assert_eq!(buf0[5], 5);
-
-        let maybe_buf1 = memory.alloc_mut(10);
-        assert_ne!(maybe_buf1, None);
-        let buf1 = maybe_buf1.unwrap();
-        assert_eq!(buf1.len(), 10);
-        assert_eq!(buf1[0], 12);
-        assert_eq!(buf1[1], 13);
-        buf1[0] = 255;
-        assert_eq!(buf1[0], 255);
-
-        assert_eq!(memory.remaining(), 3);
-
-        let bytes3 = memory.alloc(20);
-        assert_eq!(None, bytes3);
-        assert_eq!(memory.remaining(), 3);
     }
 }
--- wasm/src/packet/packetHeader.rs
+++ wasm/src/packet/packetHeader.rs
@@ -1,17 +1,18 @@
-extern crate buffer;
-use buffer::BufferRef;
+extern crate byteorder; // 1.2.7
 use num_derive::FromPrimitive;    
 use num_traits::FromPrimitive;
+use byteorder::ByteOrder;
 
-const S : u32 = 83;// utils::stringutil::get_char_code(&"S"); 83
-const D : u32 = 68;// utils::stringutil::get_char_code(&"D"); 68
-const C : u32 = 67;// utils::stringutil::get_char_code(&"C"); 67
-const PACKET_MAGIC_D:u32 = (S << 8) | D; //'DS'   default=not zip
-const PACKET_MAGIC_C:u32 = (S << 8) | C; //'CS'    zip
-const HEAD_BYTES_SIZE:u32 = 2 + 4 + 4 + 4 + 4 + 8;
+use crate::buffer::buffer::Buffer;
+const S : u16 = 83;// utils::stringutil::get_char_code(&"S"); 83
+const D : u16 = 68;// utils::stringutil::get_char_code(&"D"); 68
+// const C : u8 = 67;// utils::stringutil::get_char_code(&"C"); 67
+const PACKET_MAGIC_D:u16 = (S << 8) | D; //'DS'   default=not zip
+// const PACKET_MAGIC_C:u8 = (S << 8) | C; //'CS'    zip
+const HEAD_BYTES_SIZE:u8 = 2 + 4 + 4 + 4 + 4 + 8;
 
 
-#[repr(u16)]
+#[repr(u8)]
 #[derive(FromPrimitive)]
 enum PacketHeaderKey {
     OPCODE = 0,
@@ -19,7 +20,6 @@ enum PacketHeaderKey {
     TIMESTAMP = 2,
     BLEN = 3,
     MAGIC = 4,
-
 }
 /**
    * // --- Header structure --- \\
@@ -37,102 +37,96 @@ enum PacketHeaderKey {
    * |    timestamp   +   double  +   8Bytes  +   parameter of packet |
    * +------------------------------------------------------------+
    */
-pub struct PacketHeader<'a,'b> {
-    head_len :u32,
-    len:u32,
-    offset:u32,
-    opcode:u32,
-    param:u32,
+pub struct PacketHeader {
+    head_len :u8,
+    len:u8,
+    offset:u8,
+    opcode:u8,
+    param:u8,
+    uuid:u8,
     time_stamp:u32,
-    magic:u32,
-    buf:BufferRef<'a, 'b>
+    magic:u16,
+    buf: Buffer
 }
 
-impl <'a,'b>PacketHeader<'a, 'b>{
-   pub fn new(buffer: &'a mut [u8], initialized: &'b mut usize)-> Self {
+impl PacketHeader{
+   pub fn new()-> Self {
       PacketHeader {
         head_len : HEAD_BYTES_SIZE,
         len : 0,
         offset : 0,
         opcode : 0,
         param : 0,
+        uuid:0,
         time_stamp : 0,
         magic : PACKET_MAGIC_D,
-        buf: BufferRef::new(buffer, initialized)
+        buf: Buffer::new(0)
       }
    }
 
-   pub fn set_params(&mut self , key:u8, data:u32){
+   pub fn set_params(&mut self , key:u8, data:usize){
       match FromPrimitive::from_u8(key) {
         Some(PacketHeaderKey::OPCODE)=> {
-            self.opcode = data;
+            self.opcode = u8::from_usize(data).unwrap();
         },
         Some(PacketHeaderKey::BLEN)=>{
-            self.len= data;
+            self.len= u8::from_usize(data).unwrap();
         },
         Some(PacketHeaderKey::MAGIC)=>{
-            self.magic= data;
+            self.magic= u16::from_usize(data).unwrap();
         },
         Some(PacketHeaderKey::PARAM)=>{
-            self.param= data;
+            self.param= u8::from_usize(data).unwrap();
         },
         Some(PacketHeaderKey::TIMESTAMP)=>{
-            self.time_stamp= data;
+            self.time_stamp= u32::from_usize(data).unwrap();
         },
         _=>println!("packetHeader default set"),
       }
    }
 
-   pub fn get_params(&self,key:u8)->u32{
+   pub fn get_params(&self,key:u8)->usize{
     match FromPrimitive::from_u8(key) {
         Some(PacketHeaderKey::OPCODE)=> {
-            self.opcode
+            usize::from_u8(self.opcode).unwrap()
         },
         Some(PacketHeaderKey::BLEN)=>{
-            self.len
+            usize::from_u8(self.len).unwrap()
         },
         Some(PacketHeaderKey::MAGIC)=>{
-            self.magic.into()
+            usize::from_u16(self.magic).unwrap()
         },
         Some(PacketHeaderKey::PARAM)=>{
-            self.param
+            usize::from_u8(self.param).unwrap()
         },
         Some(PacketHeaderKey::TIMESTAMP)=>{
-            self.time_stamp
+            usize::from_u32(self.time_stamp).unwrap()
         }
         None => todo!(),
       }
    }
 
-   pub fn pack(&mut self,buffer: &'a mut [u8], initialized: &'b mut usize)-> &BufferRef<'a,'b> {
-       self.clean_buf(buffer,initialized);
-
-    //    .extend(bytes.iter().cloned())
-    //    self.buf.write(self.magic)
-    //    this._buf.writeUInt16LE(this._magic, this._offset);
-    // this._offset += 2;
-
-    // this._buf.writeUInt32LE(this._len, this._offset);
-    // this._offset += 4;
-
-    // this._buf.writeUInt32LE(this._opcode, this._offset);
-    // this._offset += 4;
-
-    // this._buf.writeUInt32LE(this._uuid, this._offset);
-    // this._offset += 4;
-
-    // this._buf.writeUInt32LE(this._param, this._offset);
-    // this._offset += 4;
-
-    // this._buf.writeDoubleLE(this._timestamp, this._offset);
-    // this._offset += 8;
+   pub fn pack(&mut self)-> &Buffer {
+       self.clean_buf();
+       byteorder::NativeEndian::write_u16(&mut self.buf.bytes_mut(),self.magic);
+       self.offset += 2;
+       byteorder::NativeEndian::write_u32(&mut self.buf.bytes_mut(), u32::from_u8(self.len).unwrap());
+       self.offset +=4;
+       byteorder::NativeEndian::write_u32(&mut self.buf.bytes_mut(),u32::from_u8(self.opcode).unwrap());
+       self.offset +=4;
+       byteorder::NativeEndian::write_u32(&mut self.buf.bytes_mut(), u32::from_u8(self.uuid).unwrap());
+       self.offset +=4;
+       byteorder::NativeEndian::write_u32(&mut self.buf.bytes_mut(),u32::from_u8(self.param).unwrap());
+       self.offset +=4;
+       byteorder::NativeEndian::write_u64(&mut self.buf.bytes_mut(), u64::from_u32(self.time_stamp).unwrap());
+       self.offset +=8;
        &self.buf
    }
 
-   pub fn clean_buf(&mut self,buffer:&'a mut [u8], initialized: &'b mut usize){
-      self.buf = BufferRef::new(buffer, initialized);
+
+   pub fn clean_buf(&mut self){
+      self.buf.clear();
       self.offset = 0;
    }
-
 
 }
