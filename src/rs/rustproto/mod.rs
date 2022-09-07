0661058e70e87a28654d3f5bc21c656c5730de66
guanxiaomin <327547931@qq.com>
2022-09-07 19:30:01 +0800

    js add proto to rust

--- src/rs/rustproto/mod.rs
+++ src/rs/rustproto/mod.rs
@@ -1,3 +1,4 @@
 pub mod op_def;
 pub mod op_gameconfig;
 pub mod op_gameconfig_01;
+pub mod protoclass;
---
+++ src/rs/rustproto/protoclass.rs
@@ -0,0 +1,17 @@
+use std::any::Any;
+use serde::{Serialize, Deserialize};
+pub trait AsAny: Any {
+    fn as_any(&self)-> &dyn Any;
+}
+
+#[derive(Serialize, Deserialize,Debug)]
+pub struct ProtoClass{
+    //opcode:u8,
+}
+
+
+impl AsAny for ProtoClass {
+     fn as_any(&self) -> &dyn Any {
+        self
+     }
+}
